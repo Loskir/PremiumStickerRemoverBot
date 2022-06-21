eb9c722e8b9e29a15ee7a05884cf55b2fc06b9f6
Kirill Loskutov <kl@sinker.ru>
2022-06-22 01:13:30 +0300

    Initial commit

---
+++ deps.deno.ts
@@ -0,0 +1,1 @@
+export * from "https://deno.land/x/grammy@v1.9.0/mod.ts"
---
+++ server.ts
@@ -0,0 +1,20 @@
+import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
+import { bot } from "./bot.ts";
+import { webhookCallback } from "./deps.deno.ts";
+
+const handleUpdate = webhookCallback(bot, "std/http", {
+  secretToken: Deno.env.get("TOKEN")
+});
+
+serve({
+  "/": async (req) => {
+    if (req.method == "POST") {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    return new Response();
+  },
+});
