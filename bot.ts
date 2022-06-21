8b58a553f0196907cbbf7288dba0e45f31562fa0
Loskir <kl@sinker.ru>
2022-06-22 01:15:01 +0300

    Add main functionality

--- bot.ts
+++ bot.ts
@@ -2,6 +2,19 @@ import { Bot } from "./deps.deno.ts";
 
 export const bot = new Bot(Deno.env.get("TOKEN") || "");
 
+bot.on("message:sticker", (ctx) => {
+  if (ctx.message.sticker.premium_animation) {
+    return ctx.deleteMessage()
+      .then(() => {
+        console.log("deleted");
+      })
+      .catch((error) => {
+        console.error(error);
+        console.log("failed to delete");
+      });
+  }
+});
+
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
